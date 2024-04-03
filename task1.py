a92f32eb5dede56d760c625362fc0f72c796691c
NikBrat <nikbratushka@gmail.com>
2024-04-03 23:46:48 +0300

    Some remarks

--- task1.py
+++ task1.py
@@ -21,17 +21,18 @@ def plotting(x, y, name, option, y2=None, y3=None):
             plt.grid()
         case 2:
             fig, ax = plt.subplots(1, 2, figsize=(10, 6), layout='constrained')
-            fig.suptitle('Графики модулей $\hat{u}$\(\u03BD) и его отфильтрованного сигнала (среза)', size='x-large')
+            fig.suptitle('Графики модулей $\hat{u}$(\u03BD) и его отфильтрованного сигнала (среза)', size='x-large')
             ax[0].plot(x, np.abs(y), color='red')
             ax[1].plot(x, np.abs(y2), color='green')
             ax[0].set_xlabel('\u03BD')
             ax[0].set_ylabel('$\hat{u}$(\u03BD)')
             ax[1].set_xlabel('\u03BD')
-            ax[1].set_ylabel('$\hat{u}$\(\u03BD) (срез)')
+            ax[1].set_ylabel('$\hat{u}$(\u03BD) (срез)')
+            ax[1].set_ylim(top=np.max(y))
             ax[0].grid()
             ax[1].grid()
-            ax[0].set_title('График модуля $\hat{u}$\(\u03BD)')
-            ax[1].set_title('График модуля отфильтрованного сигнала (среза)')
+            ax[0].set_title('График модуля $\hat{u}$(\u03BD)')
+            ax[1].set_title('График модуля отфильтрованного сигнала')
         case 3:
             fig = plt.figure()
             plt.plot(x, y, color='red', label='Искаженный сигнал')
@@ -42,8 +43,18 @@ def plotting(x, y, name, option, y2=None, y3=None):
             plt.title('Графики искаженного (u(t)), отфильтрованного и исходного сигналов')
             plt.grid()
             plt.legend(loc='best')
+        case 4:
+            fig = plt.figure()
+            plt.plot(x, y2, color='green', label='|$\hat{g}$(\u03BD)|')
+            plt.plot(x, y, label='|$\hat{f}$(\u03BD)|(clipped)', linestyle='--', color='blue')
+            plt.xlabel('\u03BD')
+            plt.ylabel('|$\hat{f}$(\u03BD)|')
+            plt.title('Графики модуля образа $\hat{g}$(\u03BD) и отфильтрованного сигнала')
+            plt.grid()
+            plt.legend(loc='best')
     # plt.show()
     fig.savefig(f'{name}'.replace('.', ','), dpi=200)
+    plt.close(fig)
 
 
 def dot_product(f, g, mn, mx):
@@ -86,24 +97,27 @@ def noisy_signal(t: np.ndarray, g, rndm, b, c, d):
 
 initial_function = np.vectorize(initial_function)
 interval = np.linspace(-6, 6, 1000)
+frequencies = np.linspace(-3, 3, 1000)
 original = initial_function(interval)
+original_fourier = fourier_transformation(original)(frequencies)
 
 noisy_signal = np.vectorize(noisy_signal)
 rng = np.random.default_rng(27)
 rnd = rng.random((interval.size,))
 
-bs = [4]
-clp_freqs = [450]
-c = 1
-d = 3
+bs = [0.25, 1, 4]
+clp_freqs = [200, 300, 400]
+cs = [0]
+ds = [0]
 for b in bs:
-    received = noisy_signal(interval, original, rnd, b, c, d)
-    plotting(interval, received, f'Noisy_{b}_{c}_{d}', 1)
-    for clp_freq in clp_freqs:
-        frequencies = np.linspace(-3, 3, 1000)
-        fourier = fourier_transformation(received)(frequencies)
-        fourier_clipped = np.concatenate((fourier[0:clp_freq], np.zeros(100), fourier[-clp_freq-1:-1]))
-        plotting(frequencies, fourier, f'Fourier_Image_{b}_{c}_{d}_{frequencies[clp_freq]}', 2, y2=np.abs(fourier_clipped))
-        fourier_inverse = fourier_transformation_inverse(fourier_clipped)(interval)
-        plotting(interval, received, f'Cleaned_{b}_{c}_{d}_{frequencies[clp_freq]}', 3,  y2=fourier_inverse, y3=original)
-
+    for c in cs:
+        for d in ds:
+            received = noisy_signal(interval, original, rnd, b, c, d)
+            # plotting(interval, received, f'Noisy_{b}_{c}_{d}', 1)
+            for clp_freq in clp_freqs:
+                fourier = fourier_transformation(received)(frequencies)
+                fourier_clipped = np.concatenate((np.zeros(clp_freq), fourier[clp_freq:-clp_freq], np.zeros(clp_freq)))
+                # plotting(frequencies, np.abs(fourier),f'Fourier_Image_{b}{frequencies[clp_freq]}', 2, y2=np.abs(fourier_clipped))
+                plotting(frequencies, np.abs(fourier_clipped), f'Fourier_Image_Comparison_{b}_{frequencies[clp_freq]}', 4, y2=np.abs(original_fourier))
+                # fourier_inverse = fourier_transformation_inverse(fourier_clipped)(interval)
+                # plotting(interval, received, f'Cleaned_{b}_{c}_{d}_{frequencies[clp_freq]}', 3,  y2=fourier_inverse, y3=original)
